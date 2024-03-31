ee4693e277c8c8250559fd30c7511a40d6266894
Ihsan <ihsanalamsyah596@gmail.com>
2024-04-01 01:05:30 +0700

    try

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 import env from "dotenv";
+import mysql2 from 'mysql2';
 
 env.config();
 
 const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
 });
 
 export default db;
